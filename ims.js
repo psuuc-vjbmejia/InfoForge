5f4c47f61b1b3df1c3ff91439cfdb1b701115020
VeeJay <132413717+psuuc-vjbmejia@users.noreply.github.com>
2023-06-27 23:54:25 +0800

    Update ims.js

--- ims.js
+++ ims.js
@@ -1,120 +1,92 @@
+let data = [
+  { id: 1, name: "Dowee Donut", price: "₱97" },
+  { id: 2, name: "Fudgee Bar", price: "₱79" },
+  { id: 3, name: "ALibaba", price: "₱29" },
+  { id: 4, name: "Bawang na bawang", price: "₱29" },
+  { id: 5, name: "Bida", price: "₱29" },
+  { id: 6, name: "Maxx", price: "₱59" },
+  { id: 7, name: "Snow Bear", price: "₱69" },
+  { id: 8, name: "Mentos", price: "₱59" },
+  { id: 9, name: "Pancit Canton", price: "₱99" },
+  { id: 10, name: "Lucky Me Noodles", price: "₱89" },
 
-let data = [];
+];
 
+// Display the initial data in the table
+displayData(data);
 
-function addRow(name, email) {
-  const table = document.getElementById("table");
-  const newRow = table.insertRow();
-
-  const nameCell = newRow.insertCell();
-  nameCell.textContent = name;
-
-  const emailCell = newRow.insertCell();
-  emailCell.textContent = email;
-
-  const actionsCell = newRow.insertCell();
-  actionsCell.innerHTML = `<button class="edit-btn">Edit</button><button class="delete-btn">Delete</button>`;
-
-
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-}
-
-
-function handleFormSubmit(event) {
+// Handle form submission
+const form = document.getElementById("form");
+form.addEventListener("submit", function(event) {
   event.preventDefault();
+  const name = document.getElementById("Name").value;
+  const price = document.getElementById("Price").value;
+  addData(name, price);
+  form.reset();
+});
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
+// Handle search input
+const searchInput = document.getElementById("search");
+searchInput.addEventListener("input", function() {
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredData = data.filter(item =>
+    item.name.toLowerCase().includes(searchTerm) ||
+    item.price.toLowerCase().includes(searchTerm)
+  );
+  displayData(filteredData);
+});
 
-  if (name && email) {
-    addRow(name, email);
-    data.push({ name, email });
-  }
+// Function to add data to the table
+function addData(name, price) {
+  const id = data.length + 1;
+  const newData = { id, name, price };
+  data.push(newData);
+  displayData(data);
 }
 
-function handleEditButtonClick(event) {
-  const row = event.target.closest("tr");
-  const nameCell = row.cells[0];
-  const emailCell = row.cells[1];
-
-  const nameInput = document.createElement("input");
-  nameInput.type = "text";
-  nameInput.value = nameCell.textContent;
-  nameCell.textContent = "";
-  nameCell.appendChild(nameInput);
-
-  const emailInput = document.createElement("input");
-  emailInput.type = "text";
-  emailInput.value = emailCell.textContent;
-  emailCell.textContent = "";
-  emailCell.appendChild(emailInput);
-
-  const editButton = row.querySelector(".edit-btn");
-  editButton.textContent = "Save";
-  editButton.classList.add("save-btn");
-  editButton.classList.remove("edit-btn");
-}
-
-
-function handleSaveButtonClick(event) {
-  const row = event.target.closest("tr");
-  const nameCell = row.cells[0];
-  const emailCell = row.cells[1];
-
-  const nameInput = nameCell.querySelector("input");
-  const emailInput = emailCell.querySelector("input");
-
-  const newName = nameInput.value;
-  const newEmail = emailInput.value;
-
-  nameCell.removeChild(nameInput);
-  emailCell.removeChild(emailInput);
-
-  nameCell.textContent = newName;
-  emailCell.textContent = newEmail;
-
-  const saveButton = row.querySelector(".save-btn");
-  saveButton.textContent = "Edit";
-  saveButton.classList.add("edit-btn");
-  saveButton.classList.remove("save-btn");
-}
-
-function handleDeleteButtonClick(event) {
-  const row = event.target.closest("tr");
-  const name = row.cells[0].textContent;
-  const email = row.cells[1].textContent;
-
-  row.remove();
-
-  data = data.filter((item) => item.name !== name || item.email !== email);
-}
-
-function handleSearchInput(event) {
-  const searchTerm = event.target.value.toLowerCase();
-
-  const rows = document.querySelectorAll("#table tr");
-
-  rows.forEach((row) => {
-    const name = row.cells[0].textContent.toLowerCase();
-    const email = row.cells[1].textContent.toLowerCase();
-
-    if (name.includes(searchTerm) || email.includes(searchTerm)) {
-      row.style.display = "";
-    } else {
-      row.style.display = "none";
-    }
+// Function to display data in the table
+function displayData(data) {
+  const table = document.getElementById("table");
+  table.innerHTML = `
+    <tr>
+      <th>Item Name</th>
+      <th>Price</th>
+      <th></th>
+    </tr>
+  `;
+  data.forEach(item => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${item.name}</td>
+      <td>${item.price}</td>
+      <td>
+        <button onclick="editData(${item.id})">Edit</button>
+        <button onclick="deleteData(${item.id})">Delete</button>
+      </td>
+    `;
+    table.appendChild(row);
   });
 }
 
-document.getElementById("form").addEventListener("submit", handleFormSubmit);
-document.getElementById("table").addEventListener("click", function (event) {
-  if (event.target.classList.contains("edit-btn")) {
-    handleEditButtonClick(event);
-  } else if (event.target.classList.contains("save-btn")) {
-    handleSaveButtonClick(event);
-  } else if (event.target.classList.contains("delete-btn")) {
-    handleDeleteButtonClick(event);
+// Function to edit data
+function editData(id) {
+  const item = data.find(item => item.id === id);
+  if (item) {
+    const name = prompt("Enter new name", item.name);
+    const price = prompt("Enter new email", item.price);
+    if (name && price) {
+      item.name = name;
+      item.price = price;
+      displayData(data);
+    }
   }
-});
-document.getElementById("search").addEventListener("input", handleSearchInput);
+}
+
+// Function to delete data
+function deleteData(id) {
+  const confirmed = confirm("Are you sure you want to delete this item?");
+  if (confirmed) {
+    data = data.filter(item => item.id !== id);
+    displayData(data);
+  }
+}
