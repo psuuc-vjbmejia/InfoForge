2a2bdb90809228cf046187a81df1eabde258f0ae
VeeJay <132413717+psuuc-vjbmejia@users.noreply.github.com>
2023-06-27 23:24:21 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,23 +1,18 @@
-
 function checkRegistrationData() {
+  const predefinedEmail = "infoforge@gmail.com";
+  const predefinedPassword = "1234";
+
+  const loginForm = document.querySelector('.form');
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const loginEmail = document.getElementById('loginEmail').value;
+    const loginPassword = document.getElementById('loginPassword').value;
+
+    if (loginEmail === predefinedEmail && loginPassword === predefinedPassword) {
+      window.location.href = "dashboard.html"; 
+    } else {
+      alert("Invalid email or password. Please try again.");
+    }
+  });
 }
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-
-
-  const email = document.getElementById("loginEmail").value;
-  const password = document.getElementById("loginPassword").value;
-
-
-  const givenEmail = "infoforge@gmail.com"; 
-  const givenPassword = "infoforge123"; 
-
-  if (email === givenEmail && password === givenPassword) {
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Invalid email or password. Please try again.");
-  }
-}
-
-document.getElementById("form").addEventListener("submit", handleFormSubmit);
